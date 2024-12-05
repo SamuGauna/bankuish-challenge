14d69aa6264e069b9915ce486d1591d1d448cef7
SamuGauna <samuelgauna7@gmail.com>
2024-12-05 15:40:32 -0300

    intials configs

---
+++ src/app.imports.ts
@@ -0,0 +1,46 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import { LoggerModule } from 'nestjs-pino';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from './config/typeorm.config';
+
+export const AppImports = [
+  ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: Joi.object({
+      NODE_ENV: Joi.string()
+        .valid('development', 'test', 'staging', 'production')
+        .default('development'),
+      APP_PORT: Joi.number().default(3000),
+      LOGGER_LEVEL: Joi.string()
+        .valid('error', 'warn', 'info', 'debug', 'log', 'silent')
+        .default('debug'),
+    }),
+  }),
+  LoggerModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (config: ConfigService) => {
+      return {
+        pinoHttp: {
+          reqCustomProps: (req: Request) => ({
+            body: req.body,
+          }),
+          redact: {
+            paths: [],
+            censor: '********',
+          },
+          name: process.env.npm_package_name,
+          level: config.get('LOGGER_LEVEL'),
+          prettyPrint: false,
+        },
+      };
+    },
+  }),
+
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (config: ConfigService) => TypeOrmConfig(config),
+  }),
+];
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const TypeOrmConfig = (config: ConfigService): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: config.get('DB_HOST'),
+  port: parseInt(config.get<string>('DB_PORT'), 10),
+  username: config.get('DB_USERNAME'),
+  password: config.get('DB_PASSWORD'),
+  database: config.get('DB_NAME'),
+  entities: [`${__dirname}/../**/*.entity{.ts,.js}`], // Cargar todas las entidades
+  synchronize: config.get<boolean>('DB_SYNCHRONIZE'), // Siempre en false para producción
+  logging: ['error', 'warn'],
+});
